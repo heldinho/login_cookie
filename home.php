f0db04a08b90c91a9efdab5f0dc4deea91092d4f
heldinho <helderferreirapassos@gmail.com>
2017-09-01 15:47:28 -0300

    init login cookie
    init login cookie

---
+++ home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login com Cookie</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
+</head>
+<body>
+	<center><h1>Home Page</h1></center>
+</body>
+</html>
